ba6d3261fa586206e0e99ef6cfd3929faab79e06
Patrick Müller <58700764+muePatrick@users.noreply.github.com>
2025-04-25 12:14:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Jira Enhanced
+
+My slowly growing collection of small changes to the Jira UI.
+
+## 🔗 Installation
+
+As these updates are written as a userscript [Tampermonkey](https://www.tampermonkey.net/)
+or something similar (I have not tested this with any other extension yet) is needed as a framework.
+
+For the latest version open `jiraEnhanced.user.js` as a
+[raw file](https://github.com/muePatrick/jira-enhanced/raw/refs/heads/main/jiraEnhanced.user.js)
+which should trigger the installation process of the extension.
+
+Or go to [Releases](https://github.com/muePatrick/jira-enhanced/releases) and open the file
+`jiraEnhanced.user.js` in the assets section of the desired release.
+
+## ⚙️ Configuration
+
+To change the configuration open the installed script through the extensions dashboard and update
+the `config` object at the top of the script however you like.
+
+## 🔩 Enhancements
+
+### Unlock Dialog Width
+
+Unlock the width of dialogs (e.g. ticket preview). Useful on widescreen monitors and tickets with
+a lot of content.
+
+### Add Copy Link List Button
+
+Add a new button above the "Completed Issues" table in the "Status Report"
+section of the "Sprint Report" page. When clicked the links to all the
+completed tickets are copied to the clipboard as a link list which can
+be pasted into Confluence pages, etc. (Confluence might need an unformatted
+paste Ctrl-Shift-V, to not automatically format the linklist)
+
+![The added copy button](addCopyLinkListButton.png)
---
+++ jiraEnhanced.user.js
@@ -0,0 +1,92 @@
+// ==UserScript==
+// @name         Jira Enhanced
+// @description  Some small - maybe niche - enhancements for Jira
+// @author       muepatrick.dev
+// @namespace    https://muepatrick.dev/
+// @version      1.2
+// @match        https://*.atlassian.net/jira/software/*
+// @grant        none
+// ==/UserScript==
+
+// -- CONFIGURATION --
+const config = {
+    // Unlock the width of dialogs (e.g. ticket preview).
+    // Useful on widescreen monitors and tickets with a lot of content.
+    unlockDialogWidth: true,
+
+    // Add a new button above the "Completed Issues" table in the "Status Report"
+    // section of the "Sprint Report" page. When clicked the links to all the
+    // completed tickets are copied to the clipboard as a link list which can
+    // be pasted into Confluence pages, etc. (Confluence might need an unformatted
+    // paste Ctrl-Shift-V, to not automatically format the linklist)
+    addCopyLinkListButton: true,
+}
+// -- CONFIGURATION --
+
+
+function waitForElement(querySelector) {
+    return new Promise(resolve => {
+        const element = document.querySelector(querySelector);
+        if (element) return resolve(element);
+
+        const observer = new MutationObserver(() => {
+            const element = document.querySelector(querySelector);
+            if (element) {
+                observer.disconnect();
+                resolve(element);
+            }
+        })
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true,
+        })
+    })
+}
+
+function unlockDialogWidth() {
+    let observer = new MutationObserver((mutationsList) => {
+        for (let mutation of mutationsList) {
+            if (mutation.type == 'childList') {
+                let sections = document.querySelectorAll('section[role="dialog"]');
+                for (let i = 0; i < sections.length; i++) {
+                    sections[i].style.width = '100%';
+                }
+            }
+        }
+    });
+    observer.observe(document.body, { attributes: true, childList: true, subtree: true });
+}
+
+function addCopyLinkListButton() {
+    const tableQuery = 'table[aria-label="Completed Issues"]'
+    waitForElement(tableQuery).then(e => {
+        const handleClick = async () => {
+            event.preventDefault();
+            const linkElems = [...document.querySelector(tableQuery)?.querySelectorAll('a')];
+            if (!linkElems?.length) return;
+            const linkList = linkElems.map(a => {
+                const href = a.getAttribute("href");
+                if (!href) return '';
+                return `${document.location.protocol}//${document.location.host}${href}`
+            });
+            await navigator.clipboard.writeText(linkList.join('\n'));
+            document.getElementById('copy-button').textContent = '✅ Links copied!';
+
+        }
+
+        var button = document.createElement('button');
+        button.id = 'copy-button';
+        button.textContent = '🔗 Copy tickets as linklist';
+        button.onclick = handleClick;
+        document.querySelector(tableQuery).insertAdjacentElement('beforebegin', button);
+    })
+}
+
+(async function() {
+    'use strict';
+    console.log('Jira Enhanced Script loaded')
+
+    config.unlockDialogWidth && unlockDialogWidth();
+    config.addCopyLinkListButton && addCopyLinkListButton();
+})()
